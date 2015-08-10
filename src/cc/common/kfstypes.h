e99499c6711c8a422fafb4abc9e7b8848939171f
Subrata Mitra <subrata4096@gmail.com>
2015-08-10 12:04:33 -0400

    changing chunk size to 32

--- src/cc/common/kfstypes.h
+++ src/cc/common/kfstypes.h
@@ -74,7 +74,7 @@ const size_t     kKfsSTierCount = size_t(kKfsSTierMax) + 1;
 //extern size_t CHUNKSIZE = 64u << 20; //!< (64MB)
 const size_t CHUNKSIZE = 32u << 20; //!< (64MB)
 //extern size_t CHUNK_READ_SIZE = CHUNKSIZE; //!< (64MB), making it equal to CHUNKSIZE //subrata : previously it was 1MB
-const size_t CHUNK_READ_SIZE = 64u << 20; //!< (64MB), making it equal to CHUNKSIZE //subrata : previously it was 1MB
+const size_t CHUNK_READ_SIZE = 32u << 20; //!< (64MB), making it equal to CHUNKSIZE //subrata : previously it was 1MB
 
 const int MAX_RPC_HEADER_LEN = 16 << 10; //!< Max length of header in RPC req/response
 const size_t MAX_FILE_NAME_LENGTH = 4 << 10;
